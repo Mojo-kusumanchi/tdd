6086200430994d98c93b6f2af0e161942f955d4d
Mojo-kusumanchi <mojo@cloudkarya.com>
2024-06-22 12:20:56 +0000

    tdd coverage

--- project/app/models/pydantic.py
+++ project/app/models/pydantic.py
@@ -2,7 +2,7 @@
 
 
 from pydantic import BaseModel
-
+from pydantic import BaseModel, AnyHttpUrl
 
 class SummaryPayloadSchema(BaseModel):
     url: str
@@ -10,3 +10,9 @@ class SummaryPayloadSchema(BaseModel):
 
 class SummaryResponseSchema(SummaryPayloadSchema):
     id: int
+
+class SummaryUpdatePayloadSchema(SummaryPayloadSchema):
+    summary: str
+
+class SummaryPayloadSchema(BaseModel):
+    url: AnyHttpUrl
